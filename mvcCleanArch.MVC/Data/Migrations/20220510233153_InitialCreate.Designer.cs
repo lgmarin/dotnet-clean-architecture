a373dc5137bc9b5b5f5d99c1f6da5b4616b28961
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-05-10 20:33:40 -0300

    Database Migration

--- mvcCleanArch.MVC/Data/Migrations/20220510233153_InitialCreate.Designer.cs
+++ mvcCleanArch.MVC/Data/Migrations/20220510233153_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using mvcCleanArch.MVC.Data;
 
@@ -10,9 +11,10 @@ using mvcCleanArch.MVC.Data;
 namespace mvcCleanArch.MVC.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220510233153_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.2");

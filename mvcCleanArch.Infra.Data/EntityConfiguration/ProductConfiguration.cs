e50f59c7e58d3d39e500bcb86d2610d54538ade2
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-05-13 11:31:14 -0300

    DB First Data

--- mvcCleanArch.Infra.Data/EntityConfiguration/ProductConfiguration.cs
+++ mvcCleanArch.Infra.Data/EntityConfiguration/ProductConfiguration.cs
@@ -11,5 +11,29 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
        builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
        builder.Property(p => p.Description).HasMaxLength(100).IsRequired();
        builder.Property(p => p.Price).HasPrecision(10,2);
+
+       builder.HasData(
+           new Product
+            {
+                Id = 1,
+                Name = "Caderno",
+                Description = "Caderno Espiral 100 Folhas Capa Felipe Dylon",
+                Price = 9.45m
+            },
+            new Product
+            {   
+                Id = 2,
+                Name = "Lapis",
+                Description = "Lapis preto",
+                Price = 0.45m
+            },
+            new Product
+            {   
+                Id = 3,
+                Name = "Caneta",
+                Description = "Caneta Azul",
+                Price = 1.45m
+            }
+       );
    }
 }

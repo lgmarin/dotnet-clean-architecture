b3104ee9fd0623e8f1afc7b35fbc82c7e3d66b38
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-05-16 10:07:39 -0300

    Configure Dependency Injection

--- mvcCleanArch.Infra.IOC/DependencyInjection.cs
+++ mvcCleanArch.Infra.IOC/DependencyInjection.cs
@@ -3,6 +3,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using mvcCleanArch.Infra.Data.Context;
 using Microsoft.AspNetCore.Identity;
+using mvcCleanArch.Domain.Interfaces;
+using mvcCleanArch.Infra.Data.Repositories;
+using mvcCleanArch.Application.Interfaces;
+using mvcCleanArch.Application.Services;
 
 namespace mvcCleanArch.Infra.IOC;
 
@@ -20,6 +24,10 @@ public static class DependencyInjection
 
         services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
             .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        services.AddScoped<IProductRepository, ProductRepository>();
+
+        services.AddScoped<IProductService, ProductService>();
 
         return services;
     }
